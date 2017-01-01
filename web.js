232600bd66ad4d96bba8e16728f90080ac5987d7
Iván Zsolt <ivanzsolt@Ballo-MacBook.local>
2017-01-01 20:13:56 +0100

    a

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var express = require("express");
 var logfmt = require("logfmt");
 var url = require('url');
 var redis = require('redis');
+var httpProxy = require('http-proxy');
 
 var redisURL = url.parse(process.env.REDISCLOUD_URL);
 var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
@@ -55,5 +56,5 @@ app.get('/command', function(req, res) {
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("Listening on hello " + port);
 });
