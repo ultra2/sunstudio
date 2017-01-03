af5ecafaf48a9cf9ee14a57813af2ff0209ad2ca
Iván Zsolt <ivanzsolt@Ballo-MacBook.local>
2017-01-04 00:47:03 +0100

    a

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var logfmt = require("logfmt");
 var url = require('url');
 var redis = require('redis');
 var httpProxy = require('http-proxy');
-var proxy = require('express-http-proxy');
+//var proxy = require('express-http-proxy');
+
+expressLogging = require('express-logging'),
+logger = require('logops');
 
 //var redisURL = url.parse(process.env.REDISCLOUD_URL);
 //var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
@@ -22,16 +25,31 @@ var ProxyDebug = httpProxy.createProxyServer({
 })
 
 var app = express();
+//var app = require('express-ws-routes')(); // Create an express app with websocket support 
 
-//app.use('/debug', ProxyDebug.ws.bind(ProxyDebug));
 
-app.use('/debug', proxy('ws://localhost:9229', {
-  filter: function(req, res) {
-     console.log(req.protocol)
-     return true
-  }
-}));
+app.use(expressLogging(logger));
 
+//app.websocket('/:id', function(info, cb, next) {
+    // `info` is the same as ws's verifyClient 
+//    console.log(
+//        'ws req from %s using origin %s',
+//        info.req.originalUrl || info.req.url,
+//        info.origin
+//    );
+ 
+    // Accept connections by passing a function to cb that will handle the connected websocket 
+ //   cb(function(socket) {
+ //       ProxyDebug.ws(info.req, socket);
+ //   });
+//});
+
+//app.use('/:id', ProxyDebug.web.bind(ProxyDebug));
+
+//app.get('/:param', function(req, res) {
+//  console.log("ProxyDebug.web")
+//  ProxyDebug.web(req, res);
+//});
 
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
@@ -78,6 +96,15 @@ app.get('/command', function(req, res) {
   }
 });
 
-app.listen(port, function() {
+var server = require('http').createServer(app);
+server.on('upgrade', function (req, socket, head) {
+  ProxyDebug.ws(req, socket, head);
+});
+server.listen(port, function() {
   console.log("Listening on hello " + port);
 });
+
+// Using app.listen will also create a require('ws').Server 
+//app.listen(port, function() {
+//  console.log("Listening on hello " + port);
+//});
