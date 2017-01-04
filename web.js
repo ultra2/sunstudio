861d7a250438c097f72fc468da861edb3fa1ef79
Iván Zsolt <ivanzsolt@Ballo-MacBook.local>
2017-01-04 01:04:23 +0100

    a

--- web.js
+++ web.js
@@ -8,9 +8,9 @@ var httpProxy = require('http-proxy');
 expressLogging = require('express-logging'),
 logger = require('logops');
 
-//var redisURL = url.parse(process.env.REDISCLOUD_URL);
-//var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-//client.auth(redisURL.auth.split(":")[1]);
+var redisURL = url.parse(process.env.REDISCLOUD_URL);
+var client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+client.auth(redisURL.auth.split(":")[1]);
 
 var port = Number(process.env.PORT || 5000);
 
